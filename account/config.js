cf4070316bda0025a7f15c0258439a4ae5538bc7
Zoggo1 <toggokatze@gmail.com>
2026-05-01 15:19:57 +0200

    Delete account/config.js

--- account/config.js
+++
@@ -1,4 +0,0 @@
-// config.js im Ordner /account/
-const supabaseUrl = 'https://hlcapqbkwfxlvrdedhsn.supabase.co'; // Deine URL aus "General"
-const supabaseKey = 'sb_publishable_hhaNeuG7r1pV7U-A4djxuQ_p55N_-hJ'; // Der Key aus deinem Screenshot
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
