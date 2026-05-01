2228be62989ebc23ab8261af475013ffb7c1d67c
Zoggo1 <toggokatze@gmail.com>
2026-05-01 13:03:38 +0200

    register

---
+++ account/config.js
@@ -0,0 +1,4 @@
+// config.js im Ordner /account/
+const supabaseUrl = 'https://hlcapqbkwfxlvrdedhsn.supabase.co'; // Deine URL aus "General"
+const supabaseKey = 'sb_publishable_hhaNeuG7r1pV7U-A4djxuQ_p55N_-hJ'; // Der Key aus deinem Screenshot
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
